2c0808394126f35389ed941bb77958254dff0023
ChrishonCrum <chrishonkcrum@yahoo.com>
2022-04-04 16:25:10 -0500

    commit tree 65c491b60a0c5629765a7955573927381981672e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42    
+    }
+    else { return 42 - blocks
+
+    }
+
+    
+
+}
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * 264
+}
+function distanceTravelledInFeet(start, end) {
+if (start > end){
+    return (start - end) * 264
+}
+else {
+    return (end - start) * 264
+}
+
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start,destination) <= 400) { 
+        return 0
+    }
+    else if (distanceTravelledInFeet(start,destination) >= 400 && (distanceTravelledInFeet(start,destination) <= 2000)) {
+        return (distanceTravelledInFeet(start,destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start,destination) > 2500) {
+        return 'cannot travel that far'
+        
+        }
+    else if (distanceTravelledInFeet(start,destination) > 2000) {
+        return 25 
+    }
+
+
+}
